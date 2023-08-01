756fea2b904552fba8c4166b863d7cfecfaa866d
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-01 10:12:51 -0700

    starting point of project with nav bar, menu drop down not functional yet

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href='#' className='logo'>
+          <i className='bx bxs-home-heart'></i>
+          <span>Nate Mueller</span>
+        </a>
+
+        <ul className='navbar'>
+          <li>
+            <a href='#' className='active'>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+
+        <div className='main'>
+          <a href='#'>Resume</a>
+          <div className='bx bx-menu' id='menu-icon'></div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
