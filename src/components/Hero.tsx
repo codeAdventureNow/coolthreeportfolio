1a851c5a1d753e76c63f272abd41a4c0185ae721
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-04 14:41:24 -0700

    Hero Section is lined up in mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,13 @@ export default function Hero() {
   return (
     <section id='hero'>
       <img src={Profile} alt='profilepic' id='hero-pic' />
-      <h2>Front End Developer</h2>
-      <p>
+      <h1 className='hero-elements'>Front End Developer</h1>
+      <p className='hero-elements'>
         I am a Front End Web Developer with over a year and a half of coding
         experience, specializing in crafting modern web applications using
         React, TypeScript, JavaScript, HTML, and CSS.
       </p>
-      <div className='social-profile'>
+      <div className='social-profile hero-elements'>
         <a
           rel='noreferrer'
           href='https://www.linkedin.com/in/nate-mueller-b50317/'
@@ -26,33 +26,8 @@ export default function Hero() {
           <i className='fa fa-github fa-inverse'></i>
         </a>
       </div>
-      <div className='techstack'>
-        <h3>Tools</h3>
+      <div className='techstack hero-elements'>
         <p>
-          <img
-            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'
-            alt='css3'
-            width='40'
-            height='40'
-          />{' '}
-          <img
-            src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
-            alt='git'
-            width='40'
-            height='40'
-          />
-          <img
-            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
-            alt='html5'
-            width='40'
-            height='40'
-          />
-          <img
-            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
-            alt='javascript'
-            width='40'
-            height='40'
-          />
           <img
             src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
             alt='react'
@@ -65,6 +40,32 @@ export default function Hero() {
             width='40'
             height='40'
           />
+          <img
+            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+            alt='javascript'
+            width='40'
+            height='40'
+          />
+          <img
+            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
+            alt='html5'
+            width='40'
+            height='40'
+          />
+          <img
+            src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'
+            alt='css3'
+            width='40'
+            height='40'
+          />
+
+          <img
+            src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
+            alt='git'
+            width='40'
+            height='40'
+          />
+
           <img
             src='https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg'
             alt='redux'
