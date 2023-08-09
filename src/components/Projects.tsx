399ea0dd275b3dd9f005c4dd139774ecf2dd8f6a
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-09 12:02:11 -0700

    card now has source code and live buttons

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,30 @@
 import IdeaBoard from '../assets/IdeaBoard.jpg';
 
-function ProjectCard({ img, title, description }) {
+function ProjectCard({ img, title, description, hrefApp, hrefSource }) {
   return (
     <div className='project-card'>
       <img src={img} alt={title} />
       <div className='card-text'>
         <h2>{title}</h2>
         <p>{description}</p>
+        <div className='card-buttons'>
+          <a
+            rel='noreferrer'
+            className='cta-btn'
+            target='_blank'
+            href={hrefApp}
+          >
+            See Live
+          </a>
+          <a
+            rel='noreferrer'
+            target='_blank'
+            className='cta-btn'
+            href={hrefSource}
+          >
+            Source Code
+          </a>
+        </div>
       </div>
 
       <div className='project-text'></div>
@@ -26,6 +44,8 @@ export default function Projects() {
             description={
               'A CRUD app for storing ideas and sorting them by date or alphabetically.'
             }
+            hrefApp={'https://idea-board-2-0.vercel.app/'}
+            hrefSource={'https://github.com/codeAdventureNow/Idea-Board-2.0'}
           />
         </div>
       </div>
